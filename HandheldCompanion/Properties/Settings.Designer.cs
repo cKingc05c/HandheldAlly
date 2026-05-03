18d686dacb9217130b59c5cc146d2abf3ca75636
Lesueur Benjamin <benjamin.lesueur@live.com>
2026-05-03 19:26:16 +0200

    Set LayoutMode default value to 2

--- HandheldCompanion/Properties/Settings.Designer.cs
+++ HandheldCompanion/Properties/Settings.Designer.cs
@@ -1201,7 +1201,7 @@ namespace HandheldCompanion.Properties
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("2")]
         public int LayoutMode {
             get {
                 return ((int)(this["LayoutMode"]));
